dc3e3f52ddcad7ff4727813ca121edd1386e9136
Daniel Lipowczan <daniel.lipowczan@gmail.com>
2020-03-21 11:36:40 +0100

    commit

---
+++ P4_Lab4_cz2/Program.cs
@@ -0,0 +1,52 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace P4_Lab4_cz2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            var google = new Website("http://google.pl", "/");
+            var ath = new Website("http://ath.bielsko.pl", "/");
+            var wikipedia = new Website("http://pl.wikipedia.org", "/wiki/.Net_Core");
+            var yt = new Website("http://youtube.com", "/");
+            var ath2 = new Website("http://ath.bielsko.pl", "/graficzne-formy-przekazu-informacji");
+
+            var tasks = new List<Task<string>>();
+
+            stopwatch.Start();
+             tasks.Add(google.Download());
+            Console.WriteLine(stopwatch.Elapsed);
+             tasks.Add(ath.Download());
+            Console.WriteLine(stopwatch.Elapsed);
+             tasks.Add(wikipedia.Download());
+            Console.WriteLine(stopwatch.Elapsed);
+             tasks.Add(yt.Download());
+            Console.WriteLine(stopwatch.Elapsed);
+             tasks.Add(ath2.Download());
+            Console.WriteLine(stopwatch.Elapsed);
+
+            Console.WriteLine("---------------");
+
+            await Task.WhenAny(tasks);
+            Console.WriteLine(stopwatch.Elapsed);
+            var htmls=await Task.WhenAll(tasks);
+            Console.WriteLine(stopwatch.Elapsed);
+
+            foreach (var html in htmls)
+            {
+                Console.WriteLine(html.Length);
+            }
+
+            //var html = google.Download();
+            //Console.WriteLine(stopwatch.Elapsed);
+            //Console.WriteLine(html);
+            //Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
---
+++ P4_Lab4_cz2/Website.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace P4_Lab4_cz2
+{
+    public class Website
+    {
+        public Website(string domain,string path, Method method=Method.GET)
+        {
+            _restClient = new RestClient(domain);
+            _restRequest = new RestRequest(path, method);
+        }
+
+        private RestClient _restClient;
+        private RestRequest _restRequest;
+
+        public async Task<string> Download()
+        {
+            var respone = await _restClient.ExecuteAsync(_restRequest);
+            return respone.Content;
+        }
+    }
+}
